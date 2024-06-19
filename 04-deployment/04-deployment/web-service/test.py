4d101beb09b513daea59ad778f8e18a851c4cf53
erinyep <erinyepis@gmail.com>
2024-06-19 14:07:07 +0000

    updates

--- 04-deployment/04-deployment/web-service/test.py
+++ 04-deployment/04-deployment/web-service/test.py
@@ -1,4 +1,4 @@
-import requests
+import predict
 
 ride = {
     "PULocationID": 10,
@@ -6,6 +6,6 @@ ride = {
     "trip_distance": 40
 }
 
-url = 'http://localhost:9696/predict'
-response = requests.post(url, json=ride)
-print(response.json())
+features = predict.prepare_features(ride)
+pred = predict.predict(features)
+print(pred)
